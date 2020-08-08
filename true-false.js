1acfe87c35d1968f5df63070f2b549ce87696be5
Joyntu847 <chowdhuryrasel040@gmail.com>
2020-08-08 21:55:56 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy value:
+// 0
+// ""
+// false
+// null
+// NaN
+// undefined
+// Truthy value:
+// "0"
+// " "
+// []
+// {}
+const name= {};
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
