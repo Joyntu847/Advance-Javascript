8050c5fd5003c50f02344e3c94379032cbb01e27
Joyntu847 <chowdhuryrasel040@gmail.com>
2020-08-08 22:48:59 +0600

    null vs undefined clearedn for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2)
+    return
+}
+let result=add(23,34);
+ console.log(result);
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+let result=add(23);
+console.log(result);
+
+const friend={name:"pranto",age:"23"}
+console.log(friend.gfName)
+
+let fan= undefined;
+console.log(fan);
+
+let ages=[23,12,34,56];
+console.log(ages[8])
